bb7f2fb6848882ee0f698ec292f67a314d46b80b
Amey Rokde <Amey.Rokde@veritas.com>
2019-07-26 16:40:12 +0530

    Added functions getAll and get and post to user controller

--- controllers/user.go
+++ controllers/user.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"net/http"
 	"regexp"
+
+	"github.com/ameyrokde/webservice/models"
 )
 
 type userController struct {
@@ -13,8 +15,40 @@ func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from User Controller"))
 }
 
+func (uc userController) getAll(w http.ResponseWriter, r *http.Request) {
+	encodeResponseAsJSONWithList(models.GetUsers(), w)
+}
+
+func (uc userController) get(id int, w http.ResponseWriter) {
+	u, err := models.GetUserByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	encodeResponseAsJSON(u, w)
+}
+
+func (uc userController) parseRequest(r *http.Request) (models.User, error) {
+	return models.User{}, nil
+}
+
+func (uc userController) post(w http.ResponseWriter, r *http.Request) {
+	u, err := uc.parseRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	encodeResponseAsJSON(u, w)
+}
+
 func newUserController() *userController {
 	return &userController{
 		userIDPattern: regexp.MustCompile("^/users/(\\d+)/?"),
 	}
+}
+
+func encodeResponseAsJSON(user models.User, w http.ResponseWriter) {
+
+}
+
+func encodeResponseAsJSONWithList(users []*models.User, w http.ResponseWriter) {
+
 }
